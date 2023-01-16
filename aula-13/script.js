a7234db4d3f4f0ca8afcc00fe804ae72dcdcd24e
RodrigoFN <107443321+Rodrigo-FN@users.noreply.github.com>
2023-01-16 13:26:01 -0300

    save

---
+++ aula-13/script.js
@@ -0,0 +1,36 @@
+/*var c = 1 
+
+do {
+    document.write(`Passo ${c} <br>`)  // REPETIÇÃO SIMPLES (DO WHILE)
+    c++
+} while (c <= 20)*/
+
+/*
+while (c <= 20) {
+    document.write(`Passo ${c} <br>`)   // REPETIÇÃO SIMPLES (WHILE)
+    c++
+}
+*/
+
+/*console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')*/
+
+var x = 1
+
+while (x <= 20) {
+    document.write(`Passo ${x} <br>`)   // REPETIÇÃO SIMPLES (WHILE)
+    x++
+}
+
+
+var ola = 1 
+
+while (ola <= 10) {
+    document.write(`Olá ${ola} <br>`)
+    ola++
+}
---
+++ aula-14/script.js
@@ -0,0 +1,7 @@
+var c = 1
+
+for (c = 1; c <= 10; c++) {
+    document.write(c + "<br>") //ESTRUTURA REPETIÇÃO FOR
+}
+document.write('FIM')
+
